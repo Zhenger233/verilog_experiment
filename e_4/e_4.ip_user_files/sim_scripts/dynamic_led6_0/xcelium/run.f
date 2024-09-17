e22effe975fed0ef68a5c8612d468def6c9f5f80
Zhenger233 <2042712521@qq.com>
2024-09-17 23:41:25 +0800

    rm other files

--- e_4/e_4.ip_user_files/sim_scripts/dynamic_led6_0/xcelium/run.f
+++
@@ -1,8 +0,0 @@
--makelib xcelium_lib/xil_defaultlib \
-  "../../../../Dynamic_LED_IP/Dynamic_LED_IP.srcs/sources_1/new/e_4_ip.v" \
-  "../../../../e_4.srcs/sources_1/ip/dynamic_led6_0/sim/dynamic_led6_0.v" \
--endlib
--makelib xcelium_lib/xil_defaultlib \
-  glbl.v
--endlib
-
